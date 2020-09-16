447f5eaa9b13ec3fa2b81ccf204e99e59c243842
robert kaylor <robertkaylor@roberts-MBP.home>
2020-09-16 18:58:53 -0400

    added record function to be able to view stored data and add stored data.

--- public/db.js
+++ public/db.js
@@ -8,3 +8,28 @@ request.onupgradeneeded = function(event) {
   db.createObjectStore("pending", { autoIncrement: true });
 };
 
+//create a function to target the database and check that it is online. 
+request.onsuccess = function(event) {
+    db = event.target.result;
+      if (navigator.onLine) {
+      checkDatabase();
+    }
+  };
+  // Create function to determine if the event has any error occurences 
+  request.onerror = function(event) {
+    console.log("Error " + event.target.errorCode);
+  };
+  //create  funcion "record" to store to db on pending with readwrite 
+  function saveRecord(record) {
+    const transaction = db.transaction(["pending"], "readwrite");
+  
+    // object store for pending db data to be accessed
+    const store = transaction.objectStore("pending");
+  
+    // adds the record with the store method
+    store.add(record);
+  }
+
+  
+
+  
